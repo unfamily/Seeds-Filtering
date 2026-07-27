5b5c12afbada438edb98cef087dda96477aec0b2
unfamily <mirko9876dev@gmail.com>
2026-07-27 10:44:15 +0200

    backport 1.20.1

--- src/main/java/net/unfamily/seeds_filtering/SeedsFilteringMod.java
+++ src/main/java/net/unfamily/seeds_filtering/SeedsFilteringMod.java
@@ -1,15 +1,14 @@
 package net.unfamily.seeds_filtering;
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(SeedsFilteringMod.MOD_ID)
 public class SeedsFilteringMod {
     public static final String MOD_ID = "seeds_filtering";
 
-    public SeedsFilteringMod(IEventBus modEventBus, ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    public SeedsFilteringMod() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 }
